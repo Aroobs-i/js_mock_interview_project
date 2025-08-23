98aa868d56dc2390a11f665cdbb6c3db0236928c
Aroobs-i <iftikhar8995@gmail.com>
2025-08-23 21:29:18 +0500

    interview Ui

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,8 +1,23 @@
 import Image from 'next/image'
-import React from 'react'
+import { Button } from './ui/button';
+import { cn } from '@/lib/utils';
+
+enum CallStatus{
+  INACTIVE ='INACTIVE',
+  CONNECTING = 'CONNECTING',
+  ACTIVE = 'ACTIVE',
+  FINISHED = 'FINISHED'
+}
 
 const Agent = ({userName}:AgentProps) => {
     const isSpeaking = true;
+     const callStatus = CallStatus.FINISHED;
+     const messages = [
+      'Whats Your Name? ',
+      'My name is John Doe, Nice to meet you!'
+     ];
+     const lastMessage = messages[messages.length - 1];
+
   return (
     <>
     <div className='call-view'>
@@ -18,12 +33,41 @@ const Agent = ({userName}:AgentProps) => {
         </div>
         <div className='card-border'>
               <div className='card-content'>
-               <Image src='/user-avatar.png' alt='user' width={540} height={540} className='rounded-full object-cover size-[120px]'/>
+               <Image src='/favicon.ico' alt='user' width={540} height={540} 
+               className='rounded-full object-cover size-[120px]'/>
                <h3>
                 {userName}
                </h3>
               </div>
         </div>
+    </div>
+
+     {messages.length > 0 && (
+      <div className='transcript-border'>
+        <div className='transcript'>
+            <p key={lastMessage} 
+            className={cn('transition-opacity duration-500 opacity-0', 'animate-fadeIn opacity-100')}>
+                   {lastMessage}
+            </p>
+        </div>
+      </div>
+     )}
+
+    <div className='w-full flex justify-center'>
+             {callStatus !== 'ACTIVE' ? (
+              <button className='relative btn-call'>
+                <span className={cn('animate-ping absolute rounded-full opacity-75',callStatus !== 'CONNECTING' & 'hidden')}/>
+                <span>
+                {callStatus === 'INACTIVE' || callStatus === 'FINISHED' ? 'Call' : '...'}
+                </span>
+                 
+              </button>
+             ):(
+                 <button className='btn-disconnect'>
+                   End
+                 </button>
+             )             
+             }
     </div>
     </>
   )
